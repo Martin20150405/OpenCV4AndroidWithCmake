d2d6973506bf5e64cf0a7d3ccb0f1d542173f16b
Martin20150405 <1036040418@qq.com>
2016-11-22 09:28:57 +0800

    First Commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,42 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
+#include <android/log.h>
+#define  LOG_TAG    "JNI_PART"
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG, __VA_ARGS__)
+#define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG, __VA_ARGS__)
+#define LOGF(...)  __android_log_print(ANDROID_LOG_FATAL,LOG_TAG, __VA_ARGS__)
+using namespace cv;
+using namespace std;
+extern "C"{
+jstring Java_com_martin_ads_testopencv_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+
+void Java_com_martin_ads_testopencv_MainActivity_nativeProcessFrame(JNIEnv*, jobject, jlong addrGray, jlong addrRgba) {
+    Mat &mGr = *(Mat *) addrGray;
+    Mat &mRgb = *(Mat *) addrRgba;
+    vector<KeyPoint> v;
+
+/*    LOGD("%d image size %d",mGr.rows,mGr.cols);
+    resize(mGr,mGr,Size(mGr.cols*480/mGr.rows,480));
+    LOGD("%d image new-size %d",mGr.rows,mGr.cols);*/
+
+    Ptr<FeatureDetector> detector = FastFeatureDetector::create(50);
+    detector->detect(mGr, v);
+    for (unsigned int i = 0; i < v.size(); i++) {
+        const KeyPoint &kp = v[i];
+        circle(mRgb, Point(kp.pt.x, kp.pt.y), 10, Scalar(255, 0, 0, 255));
+    }
+
+
+}
+
+}
---
+++ app/src/main/java/com/martin/ads/testopencv/MainActivity.java
@@ -0,0 +1,107 @@
+package com.martin.ads.testopencv;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String    TAG = "MainActivity";
+
+    private Mat                    mRgba;
+    private Mat                    mIntermediateMat;
+    private Mat                    mGray;
+
+    private CameraBridgeViewBase   mOpenCvCameraView;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("opencv_java3");
+        System.loadLibrary("opencv_java");
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial2_activity_surface_view);
+        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setClickable(true);
+        mOpenCvCameraView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Camera camera=((JavaCameraView)mOpenCvCameraView).getCamera();
+                if (camera!=null) camera.autoFocus(null);
+            }
+        });
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mOpenCvCameraView.enableView();
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+        mGray.release();
+        mIntermediateMat.release();
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+        nativeProcessFrame(mGray.getNativeObjAddr(), mRgba.getNativeObjAddr());
+        return mRgba;
+    }
+
+    public native String stringFromJNI();
+    public native void nativeProcessFrame(long matAddrGr, long matAddrRgba);
+}
+
+
